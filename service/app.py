97c3b8ce81995509e67107cf036d94bcdce90e5f
kartiksjsu <55044082+kartiksjsu@users.noreply.github.com>
2019-11-11 03:20:44 -0800

    WebService to pull tweets from twitter
    Pulls tweets from twitter and writes them on console. Initial Commit
    Tasks remaining :- Properly cleaning tweets and writing them at destination

--- service/app.py
+++ service/app.py
@@ -1,10 +1,106 @@
-from flask import Flask, jsonify, request, make_response
-# from sklearn.externals import joblib
-from flask_restplus import fields, Resource, Api
-
+from flask import Flask
+import re 
+import tweepy 
+from tweepy import OAuthHandler 
+from textblob import TextBlob
 app = Flask(__name__)
+class TwitterClient(object): 
+     
+    
+    def __init__(self): 
+        ''' 
+        Class constructor or initialization method. 
+        '''
+        # keys and tokens from the Twitter Dev Console 
+     
+  
+        # attempt authentication 
+        try: 
+            # create OAuthHandler object 
+            self.auth = OAuthHandler(consumer_key, consumer_secret) 
+            # set access token and secret 
+            self.auth.set_access_token(access_token, access_token_secret) 
+            # create tweepy API object to fetch tweets 
+            self.api = tweepy.API(self.auth) 
+        except: 
+            print("Error: Authentication Failed")
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+     
+    def clean_tweet(self, tweet): 
+        ''' 
+        Utility function to clean tweet text by removing links, special characters 
+        using simple regex statements. 
+        '''
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
 
+    
+    def processTweet(self,tweet):
+        # process the tweets
+
+        #Convert to lower case
+        tweet = tweet.lower()
+        #Convert www.* or https?://* to URL
+        tweet = re.sub('((www.[^\s]+)|(https?://[^\s]+))','',tweet)
+        #Convert @username to AT_USER
+        tweet = re.sub('@[^\s]+','',tweet)
+        #Remove additional white spaces
+        tweet = re.sub('[\s]+', ' ', tweet)
+        #Replace #word with word
+        tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        #trim
+        tweet = tweet.strip('\'"')
+        return tweet
+        #end
+
+
+    def get_tweets(self, query, count = 10): 
+        ''' 
+        Main function to fetch tweets and parse them. 
+        '''
+        # empty list to store parsed tweets 
+        tweets = [] 
+  
+        try: 
+            # call twitter api to fetch tweets 
+            fetched_tweets = self.api.search(q = query, count = 200,lang = "en") 
+  
+            # parsing tweets one by one 
+            for tweet in fetched_tweets: 
+                # empty dictionary to store required params of a tweet 
+                parsed_tweet = {} 
+  
+                # saving text of tweet 
+                parsed_tweet['text'] = tweet.text 
+                # saving sentiment of tweet 
+                # parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text) 
+  
+                # appending parsed tweet to tweets list 
+                if tweet.retweet_count > 0: 
+                    # if tweet has retweets, ensure that it is appended only once 
+                    if parsed_tweet not in tweets: 
+                        tweets.append(parsed_tweet) 
+                else: 
+                    tweets.append(parsed_tweet) 
+  
+            # return parsed tweets 
+            return tweets 
+        
+        except tweepy.TweepError as e: 
+            # print error (if any) 
+            print("Error : " + str(e))                                         
+
+@app.route("/")
+def getSarcasmData():
+    t = TwitterClient()
+    tweets = t.get_tweets(query ="")
+    print(len(tweets))
+    for tweet in tweets:
+        print(t.processTweet(tweet['text']))
+    return "Hello World!"
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
