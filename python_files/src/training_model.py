5859582d7ad1516e918a7543185911a45ed17fde
Mukesh Mogal <mukesh@Mukeshs-MacBook-Pro.local>
2019-11-25 18:52:15 -0800

    refactoring the structure

---
+++ python_files/src/training_model.py
@@ -0,0 +1,101 @@
+#importing required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Model
+from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+from keras.optimizers import RMSprop
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing import sequence
+from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
+from sklearn.feature_extraction.text import CountVectorizer
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+import os
+import re
+
+#loading training dataset
+training_data_file='../data/training_data/Sarcasm_Headlines_Dataset.json'
+df = pd.read_json(training_data_file, lines=True)
+
+#cleaning the data
+df = df[['headline','is_sarcastic']]
+df['headline'] = df['headline'].apply(lambda x: x.lower())
+df['headline'] = df['headline'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
+
+#training model
+for idx,row in df.iterrows():
+    row[0] = row[0].replace('rt',' ')
+    
+max_fatures = 2000
+tokenizer = Tokenizer(num_words=max_fatures, split=' ')
+tokenizer.fit_on_texts(df['headline'].values)
+X = tokenizer.texts_to_sequences(df['headline'].values)
+X = pad_sequences(X)
+Y = pd.get_dummies(df['is_sarcastic']).values
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.33, random_state = 42)
+embed_dim = 128
+lstm_out = 196
+
+model = Sequential()
+model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+model.add(SpatialDropout1D(0.4))
+model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+batch_size = 32
+history = model.fit(X_train, Y_train, epochs = 25, batch_size=batch_size, verbose = 2)
+validation_size = 1500
+
+X_validate = X_test[-validation_size:]
+Y_validate = Y_test[-validation_size:]
+X_test = X_test[:-validation_size]
+Y_test = Y_test[:-validation_size]
+score,accu = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
+print("score: %.2f" % (score))
+print("acc: %.2f" % (accu))
+pos_cnt, neg_cnt, pos_correct, neg_correct = 0, 0, 0, 0
+for x in range(len(X_validate)):
+    
+    result = model.predict(X_validate[x].reshape(1,X_test.shape[1]),batch_size=1,verbose = 2)[0]
+   
+    if np.argmax(result) == np.argmax(Y_validate[x]):
+        if np.argmax(Y_validate[x]) == 0:
+            neg_correct += 1
+        else:
+            pos_correct += 1
+       
+    if np.argmax(Y_validate[x]) == 0:
+        neg_cnt += 1
+    else:
+        pos_cnt += 1
+
+
+
+print("Sarcasm_acc", pos_correct/pos_cnt*100, "%")
+print("Non-Sarcasm_acc", neg_correct/neg_cnt*100, "%")
+
+#Saving the trained model
+#from keras.models import Sequential
+#from keras.layers import Dense
+#json_file = model.to_json()
+#model_path='../model/sarcasm_detect'
+#with open(model_path, "w") as file:
+#   file.write(json_file)
+
+# serialize weights to HDF5
+#h5_file_path='../model/sd_h5_file'
+#model.save_weights(h5_file_path)
+
+from keras.models import load_model
+
+model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+
